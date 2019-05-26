a8968096f815548d1bc58973417f26af6a0f74fc
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-26 22:18:16 +0800

    Agregando entidad dueño y mascota del ejercicio 3

---
+++ Cap.9/Entidades/Dueño.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_LibroCSharp.Cap._9.Entidades
+{
+    class Dueño
+    {
+    }
+}
---
+++ Cap.9/Entidades/Mascota.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_LibroCSharp.Cap._9.Entidades
+{
+    class Mascota
+    {
+    }
+}
