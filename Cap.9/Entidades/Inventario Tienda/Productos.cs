cab9fde9e45f5826043e82fca0b382aad4bb7463
escolasticoy489@outlook.com <escolasticoy489@outlook.com>
2019-05-26 20:36:48 +0800

    Add project files.

---
+++ Cap.9/Entidades/Inventario Tienda/Productos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_LibroCSharp.Cap._9.Entidades
+{
+    public struct Productos
+    {
+        public string Nombre;
+        public string Codigo;
+        public string Precio;
+        public string Cantidad;
+    }
+}
---
+++ Cap.9/Entidades/Inventario Tienda/ProductosT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_LibroCSharp.Cap._9.Entidades
+{
+    public class ProductosT
+    {
+        public string Nombre {get; set;}
+        public string Codigo { get; set; }
+        public string Precio { get; set; }
+        public string Cantidad { get; set;}
+    }
+}
